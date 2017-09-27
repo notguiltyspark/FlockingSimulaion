7ba4e2ac44c207a5fa4cb57d19f68d6a0f5e2176
notguiltyspark <31808526+notguiltyspark@users.noreply.github.com>
2017-09-27 19:09:00 +0300

    Update Boid.cpp

--- source/Boid.cpp
+++ source/Boid.cpp
@@ -1,79 +1,15 @@
 #include "Boid.h"
 
+
 void Boid::seek(sf::Vector2f& target) {
 	auto desired = target - location;
-	desired = maxSpeed * norm(desired);
+	desired = boid_params.maxSpeed * norm(desired);
 	auto steer = desired - velocity;
-	if (mag(steer) > maxForce) {
-		steer = norm(steer) * coheseCoef * maxForce;
+	if (mag(steer) > boid_params.maxForce) {
+		steer = norm(steer) * boid_params.coheseCoef * boid_params.maxForce;
 	}
 	applyForce(steer);
 };
-
-
-void Boid::separate(const std::vector<Boid>& v_vec) {
-	auto averageRepulsVec = sf::Vector2f{ 0,0 };
-	int cntr = 0;
-	for (auto veh : v_vec) {
-		auto d = mag(location - veh.getLocation());
-
-		if (d > 0 && d < desiredSeparation) {
-			averageRepulsVec += norm(location - veh.getLocation()) / d;
-			++cntr;
-		}
-
-	}
-	if (cntr > 0) {
-		averageRepulsVec = maxSpeed * norm(averageRepulsVec) / cntr;
-		auto steer = averageRepulsVec - velocity;
-		if (mag(steer) > maxForce)
-			steer = norm(steer) *separateCoef* maxForce;
-		applyForce(steer);
-	}
-
-}
-
-
-void Boid::align(const std::vector<Boid>& v_vec) {
-	auto averageAlignVec = sf::Vector2f{ 0,0 };
-	int cntr = 0;
-	for (auto veh : v_vec) {
-		auto d = mag(location - veh.getLocation());
-
-		if (d > 0 && d < visionRadius) {
-			averageAlignVec += veh.getVelocity();
-			++cntr;
-		}
-
-	}
-	if (cntr > 0) {
-		averageAlignVec = maxSpeed * norm(averageAlignVec) / cntr;
-		auto steer = averageAlignVec - velocity;
-		if (mag(steer) > maxForce)
-			steer = norm(steer) * alignCoef * maxForce;
-		applyForce(steer);
-	}
-}
-
-
-void Boid::cohese(const std::vector<Boid>& v_vec) {
-	auto averageCoheseVec = sf::Vector2f{ 0,0 };
-	int cntr = 0;
-	for (auto veh : v_vec) {
-		auto d = mag(location - veh.getLocation());
-
-		if (d > 0 && d < visionRadius) {
-			averageCoheseVec += veh.getLocation();
-			++cntr;
-		}
-
-	}
-	if (cntr > 0) {
-		averageCoheseVec = averageCoheseVec / cntr;
-		seek(averageCoheseVec);
-	}
-}
-
 
 void Boid::swarm(const std::vector<Boid>& v_vec) {
 	auto averageRepulsVec = sf::Vector2f{ 0,0 };
@@ -87,11 +23,11 @@ void Boid::swarm(const std::vector<Boid>& v_vec) {
 		auto otherVec = veh.getLocation();
 		auto d = mag(location - otherVec);
 
-		if (d > 0 && d < visionRadius) {
+		if (d > 0 && d < boid_params.visionRadius) {
 			averageAlignVec  += veh.getVelocity();
 			averageCoheseVec += otherVec;
 			++cntrAC;
-			if (d > 0 && d < desiredSeparation) {
+			if (d > 0 && d < boid_params.desiredSeparation) {
 				averageRepulsVec += norm(location - otherVec) / d;
 				++cntrRepuls;
 			}
@@ -99,23 +35,80 @@ void Boid::swarm(const std::vector<Boid>& v_vec) {
 	}
 
 	if (cntrRepuls > 0) {
-		averageRepulsVec = maxSpeed * norm(averageRepulsVec) / cntrRepuls;
+		averageRepulsVec = boid_params.maxSpeed * norm(averageRepulsVec) / cntrRepuls;
 		auto steer = averageRepulsVec - velocity;
-		if (mag(steer) > maxForce)
-			steer = norm(steer) *separateCoef* maxForce;
+		if (mag(steer) > boid_params.maxForce)
+			steer = norm(steer) *boid_params.separateCoef* boid_params.maxForce;
 		applyForce(steer);
 	}
 
 	if (cntrAC > 0) {
-		averageAlignVec = maxSpeed * norm(averageAlignVec) / cntrAC;
+		averageAlignVec = boid_params.maxSpeed * norm(averageAlignVec) / cntrAC;
 		auto steer = averageAlignVec - velocity;
-		if (mag(steer) > maxForce)
-			steer = norm(steer) * alignCoef * maxForce;
+		if (mag(steer) > boid_params.maxForce)
+			steer = norm(steer) * boid_params.alignCoef * boid_params.maxForce;
 		applyForce(steer);
 		
 		averageCoheseVec = averageCoheseVec / cntrAC;
 		seek(averageCoheseVec);
 	}
+}
+
+
+void parallel_swarm(const std::vector<Boid*>& v_ptr,
+	const std::vector<Boid*>::iterator& start,
+	const std::vector<Boid*>::iterator& end)
+{
+	// check if ( v_boids().empty() ) ?
+	auto& boid_params = v_ptr[0]->getBoidParams();
+	
+	for (auto ptr = start; ptr != end; ++ptr){
+		
+		auto averageRepulsVec = sf::Vector2f{ 0,0 };
+		auto averageAlignVec = averageRepulsVec;
+		auto averageCoheseVec = averageRepulsVec;
+
+		int cntrRepuls = 0;
+		int cntrAC = 0;
+
+		auto& curr_boid = **ptr; // hmm
+
+		for (auto& boid : v_ptr) {
+			auto d = mag(curr_boid.getLocation() - boid->getLocation());
+
+			if (d > 0 && d < boid_params.visionRadius) {
+				averageAlignVec += boid->getVelocity();
+				averageCoheseVec += boid->getLocation();
+				++cntrAC;
+
+				if (d < boid_params.desiredSeparation) {
+					averageRepulsVec += norm(curr_boid.getLocation() - boid->getLocation()) / d;
+					++cntrRepuls;
+				}
+			}
+
+		}
+
+		if (cntrRepuls > 0) {
+			averageRepulsVec = boid_params.maxSpeed * norm(averageRepulsVec) / cntrRepuls;
+			auto steer = averageRepulsVec - curr_boid.getVelocity();
+			if (mag(steer) > boid_params.maxForce)
+				steer = norm(steer) * boid_params.separateCoef * boid_params.maxForce;
+			curr_boid.applyForce(steer);
+		}
+
+		if (cntrAC > 0) {
+			averageAlignVec = boid_params.maxSpeed * norm(averageAlignVec) / cntrAC;
+			auto steer = averageAlignVec - curr_boid.getVelocity();
+			if (mag(steer) > boid_params.maxForce)
+				steer = norm(steer) * boid_params.alignCoef * boid_params.maxForce;
+			curr_boid.applyForce(steer);
+
+			averageCoheseVec = averageCoheseVec / cntrAC;
+			curr_boid.seek(averageCoheseVec);
+		}
+	}
+
 }
 
 
@@ -167,7 +160,7 @@ void Boid::checkEdges(const sf::RenderWindow& window) {
 
 void Boid::update(const sf::RenderWindow& window) {
 	velocity += acceleration;
-	limit(maxSpeed);
+	limit(boid_params.maxSpeed);
 	location += velocity;
 	ignoreEdges(window);
 	shape.setPosition(location);
