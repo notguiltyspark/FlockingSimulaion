e3d4c89f353a89c787ed3f5f8fcc1bea48cccafa
notguiltyspark <31808526+notguiltyspark@users.noreply.github.com>
2017-10-09 04:22:30 +0300

    Update Boid.cpp

--- source/Boid.cpp
+++ source/Boid.cpp
@@ -3,7 +3,7 @@
 
 void Boid::seek(sf::Vector2f& target) {
 	auto desired = target - location;
-	desired = boid_params.maxSpeed * norm(desired);
+	desired = boid_params.maxSpeed * norm(desired); 
 	auto steer = desired - velocity;
 	if (mag(steer) > boid_params.maxForce) {
 		steer = norm(steer) * boid_params.coheseCoef * boid_params.maxForce;
@@ -12,6 +12,9 @@ void Boid::seek(sf::Vector2f& target) {
 };
 
 void Boid::swarm(const std::vector<Boid>& v_vec) {
+
+	bool isNeighbourDetected = false;
+
 	auto averageRepulsVec = sf::Vector2f{ 0,0 };
 	auto averageAlignVec  = averageRepulsVec;
 	auto averageCoheseVec = averageRepulsVec;
@@ -31,39 +34,54 @@ void Boid::swarm(const std::vector<Boid>& v_vec) {
 				averageRepulsVec += norm(location - otherVec) / d;
 				++cntrRepuls;
 			}
+			isNeighbourDetected = true;
+		}
+		
+	}
+
+	if (isNeighbourDetected) {
+
+		if (cntrRepuls > 0) {
+			averageRepulsVec = boid_params.maxSpeed * norm(averageRepulsVec) / cntrRepuls;
+			auto steer = averageRepulsVec - velocity;
+			if (mag(steer) > boid_params.maxForce)
+				steer = norm(steer) *boid_params.separateCoef* boid_params.maxForce;
+			applyForce(steer);
+		}
+
+		if (cntrAC > 0) {
+			averageAlignVec = boid_params.maxSpeed * norm(averageAlignVec) / cntrAC;
+			auto steer = averageAlignVec - velocity;
+			if (mag(steer) > boid_params.maxForce)
+				steer = norm(steer) * boid_params.alignCoef * boid_params.maxForce;
+			applyForce(steer);
+
+			averageCoheseVec = averageCoheseVec / cntrAC;
+			seek(averageCoheseVec);
 		}
 	}
-
-	if (cntrRepuls > 0) {
-		averageRepulsVec = boid_params.maxSpeed * norm(averageRepulsVec) / cntrRepuls;
-		auto steer = averageRepulsVec - velocity;
-		if (mag(steer) > boid_params.maxForce)
-			steer = norm(steer) *boid_params.separateCoef* boid_params.maxForce;
-		applyForce(steer);
-	}
-
-	if (cntrAC > 0) {
-		averageAlignVec = boid_params.maxSpeed * norm(averageAlignVec) / cntrAC;
-		auto steer = averageAlignVec - velocity;
-		if (mag(steer) > boid_params.maxForce)
-			steer = norm(steer) * boid_params.alignCoef * boid_params.maxForce;
-		applyForce(steer);
+	else{
+		// wander behaviour
+		auto desired = location + norm(velocity) * 20; 
+		double angle = randint(628)/100.0;
 		
-		averageCoheseVec = averageCoheseVec / cntrAC;
-		seek(averageCoheseVec);
+		auto target = desired + sf::Vector2f(1 * cos(angle), 1 * sin(angle));
+
+		seek(target);
+		
 	}
 }
-
 
 void parallel_swarm(const std::vector<Boid*>& v_ptr,
 	const std::vector<Boid*>::iterator& start,
 	const std::vector<Boid*>::iterator& end)
 {
+	bool isNeighbourDetected = false;
 	// check if ( v_boids().empty() ) ?
 	auto& boid_params = v_ptr[0]->getBoidParams();
-	
-	for (auto ptr = start; ptr != end; ++ptr){
-		
+
+	for (auto ptr = start; ptr != end; ++ptr) {
+
 		auto averageRepulsVec = sf::Vector2f{ 0,0 };
 		auto averageAlignVec = averageRepulsVec;
 		auto averageCoheseVec = averageRepulsVec;
@@ -85,27 +103,41 @@ void parallel_swarm(const std::vector<Boid*>& v_ptr,
 					averageRepulsVec += norm(curr_boid.getLocation() - boid->getLocation()) / d;
 					++cntrRepuls;
 				}
+				isNeighbourDetected = true;
 			}
 
 		}
+		if (isNeighbourDetected) {
+			if (cntrRepuls > 0) {
+				averageRepulsVec = boid_params.maxSpeed * norm(averageRepulsVec) / cntrRepuls;
+				auto steer = averageRepulsVec - curr_boid.getVelocity();
+				if (mag(steer) > boid_params.maxForce)
+					steer = norm(steer) * boid_params.separateCoef * boid_params.maxForce;
+				curr_boid.applyForce(steer);
+			}
 
-		if (cntrRepuls > 0) {
-			averageRepulsVec = boid_params.maxSpeed * norm(averageRepulsVec) / cntrRepuls;
-			auto steer = averageRepulsVec - curr_boid.getVelocity();
-			if (mag(steer) > boid_params.maxForce)
-				steer = norm(steer) * boid_params.separateCoef * boid_params.maxForce;
-			curr_boid.applyForce(steer);
+			if (cntrAC > 0) {
+				averageAlignVec = boid_params.maxSpeed * norm(averageAlignVec) / cntrAC;
+				auto steer = averageAlignVec - curr_boid.getVelocity();
+				if (mag(steer) > boid_params.maxForce)
+					steer = norm(steer) * boid_params.alignCoef * boid_params.maxForce;
+				curr_boid.applyForce(steer);
+
+				averageCoheseVec = averageCoheseVec / cntrAC;
+				curr_boid.seek(averageCoheseVec);
+			}
 		}
+		else {
+			// (!)  this part is coupled with swam in terms of wander_dist + 1 * trigon..
+			// better to BoidParams
+			// or own []func
+			auto desired = curr_boid.getLocation() + norm(curr_boid.getVelocity()) * 20;
+			double angle = randint(628) / 100.0;/// 2 * PI;
 
-		if (cntrAC > 0) {
-			averageAlignVec = boid_params.maxSpeed * norm(averageAlignVec) / cntrAC;
-			auto steer = averageAlignVec - curr_boid.getVelocity();
-			if (mag(steer) > boid_params.maxForce)
-				steer = norm(steer) * boid_params.alignCoef * boid_params.maxForce;
-			curr_boid.applyForce(steer);
 
-			averageCoheseVec = averageCoheseVec / cntrAC;
-			curr_boid.seek(averageCoheseVec);
+			auto target = desired + sf::Vector2f(1 * cos(angle), 1 * sin(angle));
+
+			curr_boid.seek(target);
 		}
 	}
 
@@ -168,7 +200,41 @@ void Boid::update(const sf::RenderWindow& window) {
 }
 
 
-void Boid::draw(sf::RenderWindow& window) {
-	window.draw(shape);
+void Boid::draw(sf::RenderWindow& window, bool drawDirection) {
+	if (!drawDirection)
+		window.draw(shape);
+	else {
+		
+		auto angle = 0.0;
+		
+		auto vx = (velocity.x >= 0) ? velocity.x : -velocity.x;
+		auto vy = (velocity.y >= 0) ? velocity.y : -velocity.y;
+		
+		if (vx == 0 )   // or < eps?
+			angle = (vy > 0) ? 0 : PI;
+		else if (velocity.x < 0 && velocity.y < 0) {
+			angle = PI / 2.0 - atan(vy / vx);
+		} 
+		else if (velocity.x < 0 && velocity.y >= 0){
+			angle = PI / 2.0 + atan(vy / vx);
+		}
+		else if (velocity.x > 0 && velocity.y >= 0) {
+			angle = 3.0 * PI / 2.0 - atan(vy / vx);
+		}
+		else if (velocity.x > 0 && velocity.y < 0) {
+			angle = 3.0 * PI / 2.0 + atan(vy / vx);
+		}
+
+		angle *= -1 ;
+
+		dotOnHead.setRotation(angle / PI * 180.0);
+		dotOnHead.setPosition(location);
+
+		shape.setRotation(angle / PI * 180.0);
+		shape.setPosition(location);
+
+		window.draw(shape);
+		window.draw(dotOnHead);
+	}
 }
   
